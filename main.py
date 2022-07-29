de79b4a075cc60376dcdde51cbd2789f1d55b4ad
cheekysim <minecraftcheekysim@gmail.com>
2022-07-29 20:40:24 +0100

    Made program search for name not pid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import psutil
 
-process = psutil.Process(4116) # Get audiodg.exe process
+pid = [proc.pid for proc in psutil.process_iter() if "audiodg" in proc.name()]
+process = psutil.Process(pid[0]) # Get audiodg.exe process
 process.cpu_affinity(cpus=[2]) # Set CPU Affinity to CPU 2
 process.nice(psutil.HIGH_PRIORITY_CLASS) # Set CPU Proiroty to High
